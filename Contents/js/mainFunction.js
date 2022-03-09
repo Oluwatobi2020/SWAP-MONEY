3d308297c48bdb1ecc3f2bd179db43dbcd552bdc
Ojedeji Oluwatobi <ojedejitobi2020@gmail.com>
2022-03-09 17:46:58 +0100

    completed the responsive implementation of the landing page

--- Contents/js/mainFunction.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-    console.log('ready');
-    $('#navbar_wraper').load("/Components/navbar.html");    
-    $('.footer_wraper').load('/components/footer.html');
-    alert( "Load was performed." );
-});
